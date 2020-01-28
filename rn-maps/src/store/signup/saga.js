2575f542c97da8dc226021cdd4d8ab3d0105b58a
simon liu <simonlyllyyy@gmail.com>
2020-01-29 10:11:56 +1100

    alert for sign up

--- rn-maps/src/store/signup/saga.js
+++ rn-maps/src/store/signup/saga.js
@@ -3,6 +3,7 @@ import { SignUpActionTypes } from "./constant"
 import axios from "../../api/config"
 import _ from "lodash"
 import { navigate } from "../../navigationService"
+import { showAlert } from "../generalAlert/action"
 
 function* handlePostSignUp(action) {
   const { email, password } = action.payload
@@ -23,12 +24,15 @@ function* handlePostSignUp(action) {
         payload: signUpResponse.data
       })
       navigate("Signin")
+      yield put(showAlert("Sign up successful !"))
     } else {
+      yield put(showAlert("Sign up failed, please try again !"))
     }
   } catch (e) {
     yield put({
       type: SignUpActionTypes.POST_SIGNUP_ACTION_FAILED
     })
+    yield put(showAlert("Sign up failed, please try again !"))
     navigate("Signup")
   }
 }
